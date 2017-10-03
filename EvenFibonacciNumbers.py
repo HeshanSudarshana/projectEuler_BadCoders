693c98c83d534ac09ebecc61bd87c19daaad141e
anjucheran <anjucheran@outlook.com>
2017-10-04 02:57:08 +0530

    2nd Challenge Anju

--- EvenFibonacciNumbers.py
+++ EvenFibonacciNumbers.py
@@ -1,14 +1,17 @@
+#!/bin/python3
+
+import sys
+
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
+    num = 2
+    prev = 1
     tot = 0
-    a = 1
-    b = 1
-    c = 0
-    while c<=n:
-        c=a+b
-        b=a
-        a=c
-        if c%2==0 and c<=n:
-            tot+=c
-    print (tot)
+    while num<=n:
+        if num%2==0:
+            tot+=num
+        temp = prev
+        prev = num
+        num = prev + temp
+    print(tot)
